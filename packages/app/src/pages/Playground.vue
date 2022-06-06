2dbe1306d8acdecb094ce6ff27683fe49c4c2d2e
2389451262@qq.com <2389451262@qq.com>
2022-06-06 15:59:14 +0800

    chore: build structure

---
+++ packages/app/src/pages/Playground.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+</script>
+<template>
+	<div class="playground">
+		<div class="menus">
+			<button>移除左边</button>
+			<button>移除左边</button>
+		</div>
+		<div class="container">
+			<div class="box left">
+				<div class="ele"></div>
+			</div>
+			<div class="box right">
+				<div class="ele"></div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style src="@/styles/playground.less" lang="less">
+</style>
